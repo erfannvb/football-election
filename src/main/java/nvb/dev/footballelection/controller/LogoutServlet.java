5e91e89fad33a8f40953d4aa755db529eae46762
Erfan Navab <erfannavab@gmail.com>
2024-01-05 22:30:30 +0330

    implemented logout servlet

---
+++ src/main/java/nvb/dev/footballelection/controller/LogoutServlet.java
@@ -0,0 +1,23 @@
+package nvb.dev.footballelection.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+@WebServlet(name = "LogoutServlet", urlPatterns = "/logout")
+public class LogoutServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        HttpSession httpSession = req.getSession();
+        httpSession.removeAttribute("currentUser");
+        resp.sendRedirect("/login.jsp");
+
+    }
+}
